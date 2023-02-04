0350800360fd89a1ea5468f666d45dff4d8a3bac
refe <refe121080@gmail.com>
2023-02-04 08:12:55 +0000

    Added favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
-
-function App() {
+const App = () => {
   return (
 
     <div className="App">
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+const Header = () => {
+
+    return (
+
+        <Navbar bg="dark" variant="light">
+            <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
+        </Navbar>
+    )
+
+};
+
+export default Header;
