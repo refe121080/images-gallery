7e1e9cab7f0b95b64532ae1e8bf98a1f22826b1e
refe <refe121080@gmail.com>
2023-02-04 23:03:11 +0000

    list images

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 
-const ImageCard = (image) => {
+const ImageCard = ({ image }) => {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={image.urls.small} />
       <Card.Body>
         <Card.Title>{image.title}</Card.Title>
-        <Card.Text>{image.description}</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Text>{image.description || image.alt_description}</Card.Text>
+        <Button variant="primary">Delete</Button>
       </Card.Body>
     </Card>
   );
